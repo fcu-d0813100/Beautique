ae457eb334cedc947057951b0f3f949750a38443
wie <you@example.com>
2024-10-24 14:43:21 +0800

    環境清理

--- Client/pages/activity/index.js
+++ Client/pages/activity/index.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import Header from '@/components/header'
-import Styles from '@/styles/activity.scss'
-import Footer from '@/components/footer'
+import React from 'react'
+import Header from '@/components/header' // 确认路径和导出方式正确
+// import Styles from '@/styles/activity.scss';
+import Footer from '@/components/footer' // 确认路径和导出方式正确
 import Link from 'next/link'
 
-export default function Index(props) {
+export default function Activity() {
   return (
     <>
-      <Header />
-      <div className="{Style.}">index</div>
-      <Footer />
+      {/* <Header /> */}
+      <div>index</div>
+      {/* <Footer /> */}
     </>
   )
 }
